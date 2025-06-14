098a81ada39149d728b1094794c3ebd9a0180afc
YaoHaowen6355 <hy272@students.waikato.ac.nz>
2025-06-14 14:43:01 +0800

    Continue function “handle_client”
    If the file does not exist, print a "file not found" message. Get file size. Randomly select a port between 50000 and 51000 for subsequent data transmission. Set the timeout for the data socket to 5 seconds and construct the successful message. Add some necessary libraries

--- A4server.py
+++ A4server.py
@@ -1,6 +1,8 @@
 import socket
 import sys
 import threading
+import os
+from random import randint
 
 class UDPServer:
     def __init__(self, port):
@@ -10,3 +12,17 @@ class UDPServer:
         print(f"The server starts and listens on the port {self.server_port}")
 
     def handle_client(self, filename, client_address):
+        try:
+            if not os.path.exists(filename):   #Check if the requested file exists
+                error = f"ERR {filename} NOT_FOUND"
+                self.server_socket.sendto(error.encode(), client_address)
+                return
+
+        file_size = os.path.getsize(filename)#Get file size
+        data_port = randint(50000, 51000)
+        data_socket.settimeout(5) #Set the timeout for the data socket to 5 seconds
+        ok = f"OK {filename} SIZE {file_size} PORT {data_port}"
+        self.server_socket.sendto(ok.encode(), client_address)
+
+
+
